c7f60f9a277622c49a944933d564d4b4d8507acb
Jonas Kauker <jkauker@2-A-7.42heilbronn.de>
2023-11-10 14:19:17 +0100

    fixed segv and instead throw err message end exit programm (gotta finish get next line :madge:)

--- src/file_utils.c
+++ src/file_utils.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 09:14:15 by jkauker           #+#    #+#             */
-/*   Updated: 2023/11/10 11:26:16 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/10 14:18:23 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,20 @@ t_vector3	convert_to_vector3(int x, int y, int z)
 
 t_vector3	**get_map(int fd)
 {
-	t_vector3	**map;
-	int			x;
-	int			y;
+	// t_vector3	**map;
+	// int			x;
+	// int			y;
 
-	x = 0;
-	y = 0;
-	map = (t_vector3 **)malloc(sizeof(t_vector3 *));
-	/*
-	* read file line by line and realloc the **map
-	* map[x][y] and the value whats in that is z
-	*/
-	close(fd);
-	return (map);
+	// x = 0;
+	// y = 0;
+	// map = (t_vector3 **)malloc(sizeof(t_vector3 *));
+	// /*
+	// * read file line by line and realloc the **map
+	// * map[x][y] and the value whats in that is z
+	// */
+	// close(fd);
+	// return (map);
+	debug_error("get_net_line not funcional yet >:-(");
+	ft_printf("[Err] FD is %d\n", fd);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 09:13:54 by jkauker           #+#    #+#             */
-/*   Updated: 2023/11/10 12:45:26 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/10 14:16:34 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ t_map	create_map(char *map_name, int fd)
 	map.points = get_map(fd);
 	map.transform.position = (t_vector3){0, 0, 0};
 	map.transform.rotation = (t_quaternion){0, 0, 0, 0};
+	if (map.points == 0)
+	{
+		debug_error("Could not create map");
+		exit(1);
+	}
 	return (map);
 }
 
@@ -67,17 +72,16 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		debug_error("No file given\n");
+		debug_error("No file given");
 		return (RUN_ERROR);
 	}
 	vars.mlx = mlx_init(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT,
 			"FdF by jkauker", true);
 	if (!vars.mlx)
 	{
-		debug_error("Could not initialize mlx\n");
+		debug_error("Could not initialize mlx");
 		return (RUN_ERROR);
 	}
-
 	map = create_map(argv[1], open(argv[1], O_RDONLY));
 	camera = camera_create((t_vector3){0, 0, 0},
 			(t_quaternion){0, 0, 0, 0}, CAMERA_DEFAULT_FOV);
