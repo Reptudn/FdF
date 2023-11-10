c17b3e665d8c522c3ad0f923083c93d5f028d193
Jonas Kauker <jkauker@2-A-7.42heilbronn.de>
2023-11-10 14:04:03 +0100

    added functionality to move the camera up and down (if it works lol)

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 09:17:38 by jkauker           #+#    #+#             */
-/*   Updated: 2023/11/10 11:26:09 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/10 13:55:44 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	debug_error(char *error_msg)
 {
-	printf("[Err] %s\n", error_msg);
+	ft_printf("[Err] %s\n", error_msg);
 }
 
 void	debug_log(char *log_msg)
 {
-	printf("[Log] %s\n", log_msg);
+	ft_printf("[Log] %s\n", log_msg);
 }
--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 10:12:31 by jkauker           #+#    #+#             */
-/*   Updated: 2023/11/10 12:36:40 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/10 14:03:20 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,38 @@
 
 void	event_onresize(int x, int y, void *param)
 {
-	printf("[printf] Resize to x: %d\ty: %d \n", x, y);
-	printf("[printf] Params %p\n", param);
+	t_vars	*vars;
+
+	ft_printf("[printf] Resize to x: %d\ty: %d \n", x, y);
+	vars = (t_vars *)param;
+	vars->update = 1;
+	vars->window_width = x;
+	vars->window_height = y;
 }
 
 void	event_onkey(mlx_key_data_t keycode, void *param)
 {
-	printf("[printf] Key Keycode: %d\n", keycode.key);
-	printf("[printf] Params %p\n", param);
+	t_vars	*vars;
+
+	ft_printf("[printf] Key Keycode: %d\n", keycode.key);
+	vars = (t_vars *)param;
+	if (keycode.key == 53)
+		vars->run = 0;
+	else if (keycode.key == MLX_KEY_W)
+		camera_move(vars->camera, (t_transform){(t_vector3){0, 1, 0},
+			(t_quaternion){0, 0, 0, 0}});
+	else if (keycode.key == MLX_KEY_S)
+		camera_move(vars->camera, (t_transform){(t_vector3){0, -1, 0},
+			(t_quaternion){0, 0, 0, 0}});
+	vars->update = 1;
 }
 
 void	event_onmouse(mlx_key_data_t keycode, void *param)
 {
-	printf("[printf] Mouse Keycode: %d\n", keycode.key);
-	printf("[printf] Params %p\n", param);
+	t_vars	*vars;
+
+	ft_printf("[printf] Mouse Keycode: %d\n", keycode.key);
+	vars = (t_vars *)param;
+
 }
 
