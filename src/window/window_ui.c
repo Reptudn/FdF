95dbc718228437c306039e3cb55eedbc18d42318
Jonas Kauker <jkauker@1-E-8.42heilbronn.de>
2023-12-05 10:20:42 +0100

    everything works

--- src/window/window_ui.c
+++ src/window/window_ui.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:17:29 by jkauker           #+#    #+#             */
-/*   Updated: 2023/12/05 09:49:18 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/12/05 10:19:59 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	window_ui_show_controls(t_vars *vars)
 	mlx_put_string(vars->mlx, "IJKL to rotate", 10, 50);
 	mlx_put_string(vars->mlx, "ESC to exit", 10, 70);
 	mlx_put_string(vars->mlx, "Scroll to zoom", 10, 90);
-	mlx_put_string(vars->mlx, "R to reset rotation, position and zoom", 10, 110);
+	mlx_put_string(vars->mlx, "R to reset", 10, 110);
 }
